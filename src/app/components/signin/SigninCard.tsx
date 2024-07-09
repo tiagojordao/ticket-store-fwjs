157ec442f9d92a69441632ebf4aa9545a1abc0a8
Tiago Jordao <jordao.kbral@gmail.com>
2024-07-09 16:37:59 -0300

    feat: authentication

--- src/app/components/signin/SigninCard.tsx
+++ src/app/components/signin/SigninCard.tsx
@@ -10,39 +10,20 @@ export default function SigninCard() {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
 
-    const auth = (email: string, password: string) => {
-        authenticate(email, password)
-        .then(users => {
-          users.map((user: { id: string, email: string; password: string; }) => {
-            if(user.email === email) {
-                if(user.password === password){
-                    setId(user.id);
-                    return user;
-                }
-            }
-          })
+    const auth = async (email: string, password: string) => {
+        await authenticate(email, password)
+        .then(response => {
+            setId(response.id);
         })
         .catch(error => {
           throw error;
         })
-
-        redirecitIfSuccess();
-    };
-
-    useEffect(() => {
-        console.log("ID: " + id);
-    }, [id]);
-
-    const redirecitIfSuccess = () => {
-        if(id) {
-            console.log("ID: " + id);
-        }
     };
 
     return (
         <div className="signin__card bg-transparent max-w-80 w-full rounded p-6">
             <h2 className="text-xl text-white text-center mb-5">SIGN IN</h2>
-            <form action="" method="post">
+            <form id="signInForm" action="" method="post">
                 <input type="email" name="Email" id="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} className="
                     py-2 
                     px-2 
@@ -66,7 +47,7 @@ export default function SigninCard() {
                         mb-6"
                 />
                 <button type="submit" className="bg-rose-600 text-white w-full rounded hover:bg-rose-800 py-2" 
-                    onSubmit={() => auth(email, password)}
+                    onClick={() => auth(email, password)}
                 >
                     SIGN IN
                 </button>
